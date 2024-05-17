ee84c2968f2847a498398edc0d490b917ffb856a
Nakrothk <math.palkowski@gmail.com>
2024-05-17 10:44:13 -0300

    projetos

---
+++ Projeto3/src/br/edu/up/br/edu/up/App.java
@@ -0,0 +1,7 @@
+package Projeto3.src.br.edu.up.br.edu.up;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
